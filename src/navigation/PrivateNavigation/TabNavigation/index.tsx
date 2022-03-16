58b888e1211b38b6f859330add954e27077961ec
Alvaro Rene <alvaro.kom@gmail.com>
2022-03-16 17:06:53 -0600

    added vector icons

--- src/navigation/PrivateNavigation/TabNavigation/index.tsx
+++ src/navigation/PrivateNavigation/TabNavigation/index.tsx
@@ -1,16 +1,23 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { Text } from 'react-native';
+import {Text} from 'react-native';
 import {AboutScreen} from '../../../screens/AboutScreen';
 import {HomeScreen} from '../../../screens/HomeScreen';
-import { StackNavigation } from './StackNavigation';
+import {StackNavigation} from './StackNavigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigation = () => {
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="stack" component={StackNavigation} options={{tabBarIcon:()=><Text>ok</Text>}} />
+      <Tab.Screen
+        name="stack"
+        component={StackNavigation}
+        options={{
+          tabBarIcon: () => <Icon name="rocket" size={30} color="#900" />,
+        }}
+      />
       <Tab.Screen name="Anbout" component={AboutScreen} />
     </Tab.Navigator>
   );
